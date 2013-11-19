26fc44745e7180b59b07e5549d2dea71eb9b085b
App Academy Student <student@appacademy.io>
2013-11-19 10:27:36 -0500

    Initial commit, created render and redirect methods

---
+++ lib/rails_lite/controller_base.rb
@@ -0,0 +1,37 @@
+require 'erb'
+require_relative 'params'
+require_relative 'session'
+
+class ControllerBase
+  attr_reader :params
+
+  def initialize(req, res, route_params = {})
+    @req, @res = req, res
+  end
+
+  def session
+  end
+
+  def already_rendered?
+  end
+
+  def redirect_to(url)
+    @res.status = 302
+    @res["location"] = url
+
+    @already_built_response = @res
+  end
+
+  def render_content(content, type)
+    @res.body =  content
+    @res.content_type =  type
+
+    @already_built_response = @res
+  end
+
+  def render(template_name)
+  end
+
+  def invoke_action(name)
+  end
+end
