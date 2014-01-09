9c924cac4c54f6664afa95b29b484539bad012d1
Abe Schonfeld <abe.schonfeld@gmail.com>
2014-01-08 21:07:24 -0500

    finish params

--- lib/rails_lite/params.rb
+++ lib/rails_lite/params.rb
@@ -22,9 +22,34 @@ class Params
 
   private
   def parse_www_encoded_form(www_encoded_form)
-    URI.decode_www_form(www_encoded_form)
+    params = {}
+    
+    keys = URI.decode_www_form(www_encoded_form)
+    
+    keys.each do |outter_key, value|  # make sure both inner and outter keys are populated correctly
+      current_scope = params # reset to outter scope for each iteration
+      
+      key_sequence = parse_key(outter_key)
+      key_sequence.each_with_index do  |inner_key, idx|
+        
+        if (idx + 1) == key_sequence.count
+          current_scope[key] = value
+        else
+          current_scope[key] ||= {}
+          current_scope = current_scope[key]
+        end
+      end
+    end
+      
+    params
   end
 
   def parse_key(key)
+    match_data = /(?<head>.*)\[(?<rest>.*)\]/.match(key)
+    if match_data
+      parse_key(match_data["head"]).push(match_data["rest"])
+    else
+      [key]
+    end
   end
 end
